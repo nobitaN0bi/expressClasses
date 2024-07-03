6087e4c97d4ff661d3c2325ad3f31aec6c4bf05a
priyanshu <bmpriyanshu@gmail.com>
2024-07-03 11:14:50 +0530

    added delete and custom middleware to find out logging information

--- index.js
+++ index.js
@@ -15,10 +15,42 @@ app.get('/courses', (req, res) => {
 });
 
 app.post('/courses', (req, res) => {
-    const course = {id: 3, name: "hehe"};
-    courses.push(course);
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name // Assuming the request body has a 'name' property
+    }
+    courses.push(singleCourse);
+    res.json(singleCourse);
+});
+
+app.delete('/courses/delete/:id', (req, res) => {
+    // Find the course with the given id
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    // Delete the course
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the deleted course
     res.json(course);
 });
+function middleware(req, res, next) {
+    console.log("called");
+    next();
+}
+
+//logger for method,ip,hostname,date
+function logger(req, res, next) {
+    console.log("ip:", req.ip);
+    console.log("method:", req.method); 
+    console.log("hostname:", req.hostname);
+    console.log("date:", new Date()); 
+    next(); 
+}
+
+
 
 app.put('/courses/:id', (req, res) => {
     // Find the course with id 1
